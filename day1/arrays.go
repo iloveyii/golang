ed79f9f8516346c911b0d3ec68519e5d1a0f8843
Hazrat Ali <iloveyii@yahoo.com>
2021-05-05 22:57:03 +0200

    Arrays

---
+++ day1/arrays.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+	// Declare 
+	var rolls [3]int
+
+	// Assign
+	rolls[0] = 10
+	rolls[1] = 11
+	rolls[2] = 12
+
+	fmt.Println("Rolls :: ", rolls)
+
+	// One liner - can ommit 2 in [2], use len(names)
+	names := [2]string{"john", "doe"}
+	fmt.Println("Names :: ", names, len(names))
+	
+}
